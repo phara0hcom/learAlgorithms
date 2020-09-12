e526e9b033212d5ef3a6ab369e37f325428b6730
phara0hcom <t.elsay3d@gmail.com>
2020-09-12 17:32:31 +0900

    jest and nodemon

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true
+    es6: true,
+    jest: true
   },
   plugins: ['babel', 'prettier'],
   extends: ['eslint:recommended', 'plugin:prettier/recommended'],
