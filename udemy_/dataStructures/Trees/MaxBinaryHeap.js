ec76631a8689680b94ffbc2ca19d4eb00ae234ad
phara0hcom <t.elsay3d@gmail.com>
2020-10-05 09:25:43 +0900

    Section 24: MaxBinaryHeap

--- udemy_/dataStructures/Trees/MaxBinaryHeap.js
+++ udemy_/dataStructures/Trees/MaxBinaryHeap.js
@@ -7,6 +7,11 @@ class MaxBinaryHeap {
     return Math.floor((index - 1) / 2);
   }
 
+  calcChildrenIndexes(index) {
+    const times2 = index * 2;
+    return [times2 + 1, times2 + 2];
+  }
+
   push(val) {
     this.values.push(val);
     let valI = this.values.length - 1;
@@ -20,6 +25,51 @@ class MaxBinaryHeap {
       this.values[valI] = patent;
       valI = parentI;
     }
+    return this.values;
+  }
+
+  removeRoot() {
+    if (this.values.length === 0) return undefined;
+
+    const root = this.values[0];
+    if (this.values.length === 1) {
+      this.values = [];
+      return root;
+    }
+
+    // remove root
+    // take last item put it in the root position
+    const end = this.values.pop();
+    this.values[0] = end;
+
+    // sift the new root down to it's correct position
+    let index = 0;
+    const element = this.values[0];
+
+    while (true) {
+      const childrenIndexes = this.calcChildrenIndexes(index);
+      const lChildVal = this.values[childrenIndexes[0]] || Number.NEGATIVE_INFINITY;
+      const rChildVal = this.values[childrenIndexes[1]] || Number.NEGATIVE_INFINITY;
+
+      if (lChildVal < element && rChildVal < element) break;
+
+      if (lChildVal > rChildVal) {
+        this.values[index] = lChildVal;
+        index = childrenIndexes[0];
+        this.values[index] = element;
+      } else {
+        this.values[index] = rChildVal;
+        index = childrenIndexes[1];
+        this.values[index] = element;
+      }
+    }
+
+    return root;
+  }
+
+  testSetValues(newValues) {
+    this.values = [...newValues];
+
     return this.values;
   }
 }
--- udemy_/dataStructures/Trees/MaxBinaryHeap.spec.js
+++ udemy_/dataStructures/Trees/MaxBinaryHeap.spec.js
@@ -13,4 +13,57 @@ describe('MaxBinaryHeap', () => {
     expect(maxBinaryHeap.push(33)).toEqual([41, 39, 33, 12, 27, 18]);
     expect(maxBinaryHeap.push(55)).toEqual([55, 39, 41, 12, 27, 18, 33]);
   });
+
+  test('Test Push method', () => {
+    const maxBinaryHeap = new MaxBinaryHeap();
+
+    expect(maxBinaryHeap.testSetValues([70, 67, 65, 45, 58, 40, 53, 44, 15, 31])).toEqual([
+      70,
+      67,
+      65,
+      45,
+      58,
+      40,
+      53,
+      44,
+      15,
+      31
+    ]);
+
+    expect(maxBinaryHeap.removeRoot()).toEqual(70);
+    expect(maxBinaryHeap.values).toEqual([67, 58, 65, 45, 31, 40, 53, 44, 15]);
+
+    expect(maxBinaryHeap.removeRoot()).toEqual(67);
+    expect(maxBinaryHeap.values).toEqual([65, 58, 53, 45, 31, 40, 15, 44]);
+
+    expect(maxBinaryHeap.removeRoot()).toEqual(65);
+    expect(maxBinaryHeap.values).toEqual([58, 45, 53, 44, 31, 40, 15]);
+
+    expect(maxBinaryHeap.removeRoot()).toEqual(58);
+    expect(maxBinaryHeap.values).toEqual([53, 45, 40, 44, 31, 15]);
+
+    expect(maxBinaryHeap.removeRoot()).toEqual(53);
+    expect(maxBinaryHeap.values).toEqual([45, 44, 40, 15, 31]);
+
+    expect(maxBinaryHeap.removeRoot()).toEqual(45);
+    expect(maxBinaryHeap.values).toEqual([44, 31, 40, 15]);
+
+    expect(maxBinaryHeap.removeRoot()).toEqual(44);
+    expect(maxBinaryHeap.values).toEqual([40, 31, 15]);
+
+    expect(maxBinaryHeap.removeRoot()).toEqual(40);
+    expect(maxBinaryHeap.values).toEqual([31, 15]);
+
+    expect(maxBinaryHeap.removeRoot()).toEqual(31);
+    expect(maxBinaryHeap.values).toEqual([15]);
+
+    expect(maxBinaryHeap.removeRoot()).toEqual(15);
+    expect(maxBinaryHeap.values).toEqual([]);
+
+    expect(maxBinaryHeap.removeRoot()).toEqual(undefined);
+    expect(maxBinaryHeap.values).toEqual([]);
+
+    expect(maxBinaryHeap.removeRoot()).toEqual(undefined);
+    expect(maxBinaryHeap.values).toEqual([]);
+  });
 });
